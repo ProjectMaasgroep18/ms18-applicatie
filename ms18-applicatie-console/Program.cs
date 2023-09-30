5ac570fac499189b532af0362c7433ddba698548
Kevin Hartman <git@kevinhartman.nl>
2023-09-30 10:25:12 +0200

    Console app erin voor testdata

---
+++ ms18-applicatie-console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ms18_applicatie._console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
