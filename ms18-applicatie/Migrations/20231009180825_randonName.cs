08e5a0cabfecea3bc0a3773817e5780cbdcb9744
Ted-Ru <theotransoft@gmail.com>
2023-10-21 15:44:13 +0200

    Hoi

---
+++ ms18-applicatie/Migrations/20231009180825_randonName.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ms18_applicatie.Migrations
+{
+    /// <inheritdoc />
+    public partial class randonName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                schema: "receipt",
+                table: "receipt",
+                type: "numeric(2)",
+                precision: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(2,0)",
+                oldPrecision: 2,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                schema: "receipt",
+                table: "receipt",
+                type: "numeric(2,0)",
+                precision: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(2)",
+                oldPrecision: 2,
+                oldNullable: true);
+        }
+    }
+}
