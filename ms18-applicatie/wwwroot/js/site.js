7613c87c782035e6753d3174c33d75445e8264bb
rhlt <39499953+rhlt@users.noreply.github.com>
2023-11-13 16:05:08 +0100

    Photo "upload" with localStorage

--- ms18-applicatie/wwwroot/js/site.js
+++ ms18-applicatie/wwwroot/js/site.js
@@ -10,11 +10,24 @@ const LOAD_MSG = document.querySelector('#loading');
 const ERROR_MSG = document.querySelector('#error');
 
 
+function hideElement(el) {
+    // Add "hidden" class to element
+
+    if (!el.className.match(/(\s|^)hidden(\s|$)/))
+        el.className = (el.className.trim() + ' hidden').trim();
+}
+
+function showElement(el) {
+    // Remove "hidden" class from element
+    
+    el.className = el.className.replace(/(\s+|^)hidden(\s+|$)/, ' ').trim();
+}
+
 function handleError(message) {
     // Show error message
 
-    LOAD_MSG.className = 'hidden';
-    ERROR_MSG.className = '';
+    hideElement(LOAD_MSG);
+    showElement(ERROR_MSG);
     ERROR_MSG.innerText = message;
     return Promise.reject(message);
 }
@@ -31,7 +44,7 @@ async function apiGet(action, _fetchData, _skipJson) {
         redirect: "follow",
     };
 
-    LOAD_MSG.className = '';
+    showElement(LOAD_MSG);
     const json = await fetch(BASE_URL + action, _fetchData).catch(handleError).then(response => {
         if (response.status >= 400 && response.status < 600) {
             // Server error
@@ -46,7 +59,7 @@ async function apiGet(action, _fetchData, _skipJson) {
         }
         return _skipJson ? true : response.json().catch(handleError);
     });
-    LOAD_MSG.className = 'hidden';
+    hideElement(LOAD_MSG);
     return json;
 }
 
@@ -107,7 +120,7 @@ function showOutput(data, container) {
         },
     };
 
-    container.className = ''; // Unhide
+    showElement(container);
     for (key in data) {
         container.querySelectorAll('[rel="' + key + '"],[rel^="' + key + ':"]').forEach(output => {
             const [key, prop, transform] = output.getAttribute('rel').split(':');
@@ -126,3 +139,94 @@ function showOutput(data, container) {
         });
     }
 }
+
+function dropContainer(container) {
+    // Drop containers (thanks Kevin!)
+
+    const fileInput = container.querySelector('input[type="file"]');
+
+    container.addEventListener("dragover", (e) => {
+        // prevent default to allow drop
+        e.preventDefault()
+    }, false);
+
+    container.addEventListener("dragenter", () => {
+        container.classList.add("drag-active")
+    });
+
+    container.addEventListener("dragleave", () => {
+        container.classList.remove("drag-active")
+    });
+
+    container.addEventListener("drop", (e) => {
+        e.preventDefault()
+        container.classList.remove("drag-active")
+        fileInput.files = e.dataTransfer.files
+    });
+
+    container.addEventListener("click", () => fileInput.click());
+
+    fileInput.addEventListener("change", () => {
+        // Handle file selection
+        // based on: https://stackoverflow.com/questions/23945494/use-html5-to-resize-an-image-before-upload
+
+        const file = fileInput.files[0];
+        const img = container.querySelector('.drop-image');
+        const title = container.querySelector('.drop-title');
+        const reader = new FileReader();
+        const max_size = 800; // Maximum 800px (width or height)
+
+        if (file.type.match(/^image\//)) {
+            // Load the image
+
+            reader.onload = readerEvent => {
+                var image = new Image();
+                image.onload = () => {
+                    // Resize the image
+
+                    let canvas = document.createElement('canvas'),
+                        width = image.width,
+                        height = image.height;
+                    if (width > height) {
+                        if (width > max_size) {
+                            height *= max_size / width;
+                            width = max_size;
+                        }
+                    } else {
+                        if (height > max_size) {
+                            width *= max_size / height;
+                            height = max_size;
+                        }
+                    }
+                    canvas.width = width;
+                    canvas.height = height;
+                    canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+                    resizedImage = canvas.toDataURL('image/jpeg');
+
+                    // Now let's something with the resized image
+                    img.src = resizedImage;
+                    showElement(img);
+                    title.innerText = file.name;
+
+                    // Keep it in Local Storage for now
+                    try {
+                        localStorage.setItem('receipt-new', resizedImage);
+                    } catch (e) {
+                        console.warn('Local storage error', e);
+                        localStorage.clear(); // Prevent localStorage from filling up completely (hope that fixes it)
+                        localStorage.setItem('receipt-new', resizedImage);
+                    }
+                };
+                image.src = readerEvent.target.result;
+            };
+            reader.readAsDataURL(file);
+        } else {
+            // Not an image, discard it
+
+            fileInput.value = null;
+        }
+    });
+}
+
+// Init drop containers
+document.querySelectorAll('.drop-container').forEach(dropContainer);
