c42e57cdaff76e19dfb2f7674ec977e838f9893a
Kevin Hartman <git@kevinhartman.nl>
2023-12-16 13:34:26 +0100

    Kort saven ivm sequence shizzle

---
+++ ms18-database-test/ReceiptContextTest.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Maasgroep.Database.Receipts;
+using Maasgroep.Database.Test;
+using Microsoft.EntityFrameworkCore;
+
+namespace ms18_database_test
+{
+	public class ReceiptContextTest : IClassFixture<MaasgroepTestFixture>
+	{
+		public ReceiptContextTest(MaasgroepTestFixture fixture) => Fixture = fixture;
+
+		public MaasgroepTestFixture Fixture { get; }
+
+		[Fact]
+		public void Receipt_Add_A_Valid_Receipt_Result_Ok()
+		{
+			using var sut = Fixture.CreateContext();
+
+			try
+			{
+				var receipt = GetValidReceipt();
+
+				sut.Database.BeginTransaction();
+
+				var result = sut.Receipt.Add(receipt);
+				_ = result.State.Should().Be(EntityState.Added);
+
+				sut.SaveChanges();
+
+				_ = result.Entity.Id.Should().NotBe(0);
+			}
+			finally
+			{
+				sut.Database.RollbackTransaction();
+			}
+			
+			
+			
+		}
+
+
+		private Receipt GetValidReceipt()
+		{
+			return new Receipt()
+			{
+				Amount = 9876543.21M,
+				CostCentreId = 1,
+				ReceiptStatus = "Goedgekeurd",
+				Location = "",
+				Note = "Lieve Willemijn, wat ik je altijd al heb willen zeggen...",
+				MemberCreatedId = 1
+			};
+		}
+
+	}
+}
