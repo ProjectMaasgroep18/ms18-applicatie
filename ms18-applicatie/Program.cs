5abd36859e03b639f4bbe5f0743d16f12fa365e7
rhlt <39499953+rhlt@users.noreply.github.com>
2023-12-18 21:12:24 +0100

    Fix swagger route

--- ms18-applicatie/Program.cs
+++ ms18-applicatie/Program.cs
@@ -137,15 +137,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("../swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = string.Empty;
+    options.RoutePrefix = "swagger";
 });
 
 app.UseSwagger();
